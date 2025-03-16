a2956324ed4464accccfc0b54f3bfe8976e97550
Programmeramin <developeramin2615@gmail.com>
2025-03-16 23:43:51 +0600

    react and tailwind css install done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css'
+
+function App() {
+ 
+  return (
+    <>
+     <h1 className='text-3xl via-purple-500 text-green-500 p-5 text-center'>Hello Word</h1>
+    </>
+  )
+}
+
+export default App
